d4b212ac48b84be579483269f241a53e2eee26b7
Dhruv Premi <dhruvpremi@gmail.com>
2015-06-01 17:50:10 +0530

    Read All the Files and make one Big ArrayArraylist

---
+++ src/com/assign/edgeNw/ReadFiles.java
@@ -0,0 +1,63 @@
+package com.assign.edgeNw;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ReadFiles 
+{
+	BufferedReader br = null;
+	String[] paths;
+	File file = null;
+	String dataDir = "./Data/";
+	ArrayList<ArrayList<String>> allFiles = new ArrayList<ArrayList<String>>();
+	
+	ReadFiles()
+	{	
+		file = new File(dataDir);
+		paths = file.list();
+		
+		ArrayList<String> temp;
+		for(String path:paths)
+		{
+			System.out.println("\n **************** New File *******************  ");
+			
+			temp = new ArrayList<String>();
+			try 
+			{
+				String sCurrentLine;
+				br = new BufferedReader(new FileReader(dataDir + path));
+	 
+				while ((sCurrentLine = br.readLine()) != null) 
+				{
+					System.out.println(sCurrentLine);
+					temp.add(sCurrentLine);
+				}
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			} 
+			finally 
+			{
+				try 
+				{
+					if (br != null)br.close();
+				} 
+				catch (IOException ex) 
+				{
+					ex.printStackTrace();
+				}
+			}
+			allFiles.add(temp);
+		}
+	}
+	
+	public ArrayList<ArrayList<String>> getAllFileObject()
+	{
+		return allFiles;
+	}
+	
+}
--- src/com/assign/edgeNw/Start.java
+++ src/com/assign/edgeNw/Start.java
@@ -1,9 +1,23 @@
 package com.assign.edgeNw;
 
+import java.util.ArrayList;
+
 public class Start {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		
+		// First I will go to a Folder , check all the files
+		// Read them one by one and then do whatever i want 
+		
+		ReadFiles rf = new ReadFiles();
+		ArrayList<ArrayList<String>> allFiles = rf.getAllFileObject();
+		
+		// Just Check out For each File all the possible words
+		// Print them and Check it out
+		
+		
 
 	}
 
